14db9c27cc925a7444438252acd034bd817eb8c6
candinhojr <candinholuiz@gmail.com>
2017-03-20 18:44:39 -0300

    Revert "Alterações nos testes(Especificações)"
    This reverts commit ed41fc10f3b82ad8b79de82075f13b8063ca5d3a.

--- src/superarraydeinteiros/SuperArrayDeInteiros.java
+++ src/superarraydeinteiros/SuperArrayDeInteiros.java
@@ -27,39 +27,31 @@ public class SuperArrayDeInteiros {
         this.array = new ArrayInteiros(-6, -1);
         //Inicio dos testes
         //Testes Atribui
-        System.out.println("#Teste 01 - Atribui");
         try {
             this.array.atribui(-5, 50);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("#Teste 02 - Atribui");
         try {
             this.array.atribui(1, 80);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        }
-        System.out.println("#Teste 03 - Atribui");
+        } 
         try {
             this.array.atribui(0, 50);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        }
-        
-        //Testes Acesso
-        System.out.println("#Teste 01 - Acesso");
+        } 
         try {
             System.out.println("Valores: " + this.array.acessa(-5));
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        }
-        System.out.println("#Teste 02 - Acesso");
+        } 
         try {
             System.out.println("Valores: " + this.array.acessa(1));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } 
-        System.out.println("#Teste 03 - Acesso");
         try {
             System.out.println("Valores: " + this.array.acessa(-1));
         } catch (Exception e) {
